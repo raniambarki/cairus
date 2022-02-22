de90e18d979e16b4180334e321c1d635bd916d1a
mohamed l'wed <mohamed.elwed@esen.tn>
2022-02-22 14:00:57 +0100

    Update Packs.js

--- src/components/Packs/Packs.js
+++ src/components/Packs/Packs.js
@@ -63,9 +63,9 @@ export default function Packs() {
                     </div>
                 </div>
 
-                <div className="relative flex flex-col justify-between p-8 transition-shadow duration-300 bg-white border rounded shadow-sm sm:items-center hover:shadow border-deep-purple-accent-400">
+                <div className="relative flex flex-col justify-between p-8 transition-shadow duration-300 bg-white border rounded shadow-sm sm:items-center hover:shadow border-indigo-700">
                     <div className="absolute inset-x-0 top-0 flex justify-center -mt-3">
-                        <div className="inline-block px-3 py-1 text-xs font-medium tracking-wider text-white uppercase rounded bg-deep-purple-accent-400">
+                        <div className="inline-block px-3 py-1 text-xs font-medium tracking-wider text-white uppercase rounded bg-indigo-700">
                         LE PLUS POPULAIRE
                         </div>
                     </div>
@@ -84,7 +84,7 @@ export default function Packs() {
                     <div>
                         <a
                             href="/"
-                            className="inline-flex items-center justify-center w-full h-12 px-6 mt-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                            className="inline-flex items-center justify-center w-full h-12 px-6 mt-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-indigo-700 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                         >
                             Réservé            </a>
                         <p className="max-w-xs mt-6 text-xs text-gray-600 sm:text-sm sm:text-center sm:max-w-sm sm:mx-auto">
